7f227477855efa04fb57f383212be031c87943cf
Mohammed27s <mohammed.23s@outlook.com>
2024-05-02 17:37:42 +0400

    The snippet code for Shop Interface is complete

---
+++ Shop Interface.java
@@ -0,0 +1,18 @@
+package Shop;
+
+//This is Shop behaviors
+public interface ShopInterface {
+
+    void shopName();
+    void location();
+    void streetNumber();
+    void shopRevenue();
+    void shopType();
+    void shopProfit();
+    void shopExpenses();
+    void shopNetRevenue();
+    
+    
+    
+
+}
